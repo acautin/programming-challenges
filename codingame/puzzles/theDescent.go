dcbee4d39452f55cdf1dcf42bc191c6bbcecf1cc
Agustin Cautin <acautin@gmail.com>
2015-02-06 20:29:21 +0100

    Fixed format of descent solution

--- codingame/puzzles/theDescent.go
+++ codingame/puzzles/theDescent.go
@@ -1,7 +1,6 @@
 package main
 
 import "fmt"
-//import "os"
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -13,30 +12,30 @@ type pos struct {
 }
 
 func main() {
-	fired := false;
-	target := 0;
-    for {
-        var shipPos pos
-        fmt.Scan(&shipPos.x, &shipPos.y)
-        mountainHeights := make([]int, 8)
+	fired := false
+	target := 0
+	for {
+		var shipPos pos
+		fmt.Scan(&shipPos.x, &shipPos.y)
+		mountainHeights := make([]int, 8)
 
 		if shipPos.x == 0 || shipPos.x == 7 {
-			fired = false;
+			fired = false
 		}
 
-        for i := 0; i < 8; i++ {
-            // Height of one mountain, from 9 to 0.
-            fmt.Scan(&mountainHeights[i])
+		for i := 0; i < 8; i++ {
+			// Height of one mountain, from 9 to 0.
+			fmt.Scan(&mountainHeights[i])
 			if mountainHeights[i] > mountainHeights[target] {
 				target = i
 			}
-        }
-        
-        if shipPos.x == target && !fired {
+		}
+
+		if shipPos.x == target && !fired {
 			fmt.Println("FIRE")
-			fired = true;
+			fired = true
 		} else {
 			fmt.Println("HOLD")
 		}
-    }
+	}
 }
